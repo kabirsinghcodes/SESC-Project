3bdf5982820400012c1f1c1aca41ba15d670f849
kabir <kabirsingh0405@gmail.com>
2024-02-26 20:45:21 +0000

    chore(finance): :lipstick: Added "Get Invoice Page" To the App

--- finance/src/pages/about.tsx
+++ finance/src/pages/about.tsx
@@ -19,38 +19,33 @@ export default function About() {
       appearance="dark"
     >
       <div className="flex min-h-screen flex-col bg-gradient-to-r from-gray-700 via-gray-900 to-black text-white">
-        <Head />
         <div className="flex flex-col items-center justify-center flex-1">
           <Card className="mt-8 p-4" style={{ width: 370 }}>
             <Flex direction={"column"} gap={"5"}>
               <Flex direction={"column"}>
-                <h1 className="text-2xl font-semibold">Sign up</h1>
-                <p className="text-sm font-medium text-neutral-300">
+                <h1 className="text-2xl font-semibold">
+                  Fetch your Invoice here :)
+                </h1>
+                {/* <p className="text-sm font-medium text-neutral-300">
                   Create a new account
-                </p>
-              </Flex>
-              <Flex direction={"column"} gap={"1"}>
-                <Text size={"2"} className="font-semibold">
-                  Email address
-                </Text>
-                <TextField.Input placeholder="Enter your email" />
+                </p> */}
               </Flex>
               <Flex direction={"column"} gap={"1"}>
                 <Flex justify={"between"}>
                   <Text size={"2"} className="font-semibold">
-                    Password
+                    Enter Invoice Code
                   </Text>
-                  <Link size={"2"} className="font-semibold">
-                    Forgot password?
-                  </Link>
                 </Flex>
-                <TextField.Input size="3" placeholder="Enter your password" />
+                <TextField.Input
+                  size="3"
+                  placeholder="Enter your invoice code"
+                />
               </Flex>
               <Flex gap={"2"} className="place-content-end gap-2">
                 <Button variant="soft">
-                  <Text size={"2"}>Create an account</Text>
+                  <Text size={"2"}>Get Invoice</Text>
                 </Button>
-                <Button variant="solid">Sign in</Button>
+                {/* <Button variant="solid">Sign in</Button> */}
               </Flex>
             </Flex>
           </Card>
