c64b435c43e4da1d42f17f8678649182fee1a876
kabir <kabirsingh0405@gmail.com>
2024-03-03 23:31:05 +0000

    feat(finance): :tada: Backend added for finance app

---
+++ finance/backend/src/main/java/uk/ac/leedsbeckett/finance/controller/InvoiceController.java
@@ -0,0 +1,54 @@
+package uk.ac.leedsbeckett.finance.controller;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uk.ac.leedsbeckett.finance.model.Invoice;
+import uk.ac.leedsbeckett.finance.service.InvoiceService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+public
+class InvoiceController {
+
+    private final InvoiceService invoiceService;
+
+    InvoiceController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+    @GetMapping("/invoices")
+    public CollectionModel<EntityModel<Invoice>> all() {
+        return invoiceService.getAllInvoices();
+    }
+
+    @GetMapping("/invoices/{id}")
+    public EntityModel<Invoice> one(@PathVariable Long id) {
+        return invoiceService.getInvoiceById(id);
+    }
+
+    @GetMapping("/invoices/reference/{reference}")
+    public EntityModel<Invoice> one(@PathVariable String reference) {
+        return invoiceService.getInvoiceByReference(reference);
+    }
+
+    @PostMapping("/invoices")
+    ResponseEntity<?> newInvoice(@RequestBody Invoice invoice) {
+        return invoiceService.createNewInvoice(invoice);
+    }
+
+    @DeleteMapping("/invoices/{reference}/cancel")
+    public ResponseEntity<?> cancel(@PathVariable String reference) {
+        return invoiceService.cancel(reference);
+    }
+
+    @PutMapping("/invoices/{reference}/pay")
+    public ResponseEntity<?> pay(@PathVariable String reference) {
+        return invoiceService.pay(reference);
+    }
+}
